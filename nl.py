f64125df6eda548250be824fb586efbc18c3feab
stsuda007 <stsuda.indee@gmail.com>
2026-02-23 12:47:59 +0900

    Update nl.py

--- nl.py
+++ nl.py
@@ -101,7 +101,8 @@ def generate_with_gemini(final_prompt):
         return "⚠️ Google API key not found. Please add it to your environment variables."
     
     try:
-        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash:generateContent?key={GOOGLE_API_KEY}"
+        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={GOOGLE_API_KEY}"
+        
         
         payload = {
             "contents": [
